c1aae7fe9098c7adeb4d6a0959672a1bc427221d
jonnylangstone <jonny.langstone@telespazio.com>
2025-03-27 17:04:29 +0000

    refactor s3clientcredentials and added data loader

--- api/handlers/s3.go
+++ api/handlers/s3.go
@@ -16,11 +16,70 @@ import (
 	"github.com/rs/zerolog"
 )
 
-
 type STSClient interface {
 	AssumeRoleWithWebIdentity(ctx context.Context,
 		params *sts.AssumeRoleWithWebIdentityInput, optFns ...func(*sts.Options)) (
 		*sts.AssumeRoleWithWebIdentityOutput, error)
+}
+
+// getS3Credentials extracts the core logic to retrieve S3 credentials
+func GetS3Credentials(roleArn string, c STSClient, k services.KeycloakClient, r *http.Request) (S3Credentials, error) {
+	vars := mux.Vars(r)
+	workspaceID := vars["workspace-id"]
+	userID := vars["user-id"]
+
+	logger := zerolog.Ctx(r.Context()).With().Str("workspace", workspaceID).
+		Str("user", userID).Str("role arn", roleArn).Logger()
+
+	token, ok := r.Context().Value(middleware.TokenKey).(string)
+	if !ok {
+		err := fmt.Errorf("invalid token")
+		logger.Error().Msg(err.Error())
+		return S3Credentials{}, err
+	}
+
+	logger.Debug().Str("token", token).Msg("Token retrieved")
+
+	claims, ok := r.Context().Value(middleware.ClaimsKey).(authn.Claims)
+	if !ok {
+		err := fmt.Errorf("invalid claims")
+		logger.Error().Msg(err.Error())
+		return S3Credentials{}, err
+	}
+
+	logger = logger.With().Str("claims user", claims.Username).Logger()
+
+	if userID == "me" {
+		userID = claims.Username
+	}
+
+	if tokenExchangeRequired(claims, workspaceID, userID) {
+		logger.Info().Msg("Token exchange required")
+
+		workspaceToken, err := k.ExchangeToken(token, fmt.Sprintf("workspace:%s", workspaceID))
+		if err != nil {
+			logger.Error().Err(err).Msg("Failed to get offline token")
+			return S3Credentials{}, err
+		}
+		token = workspaceToken.Access
+	}
+
+	resp, err := c.AssumeRoleWithWebIdentity(r.Context(), &sts.AssumeRoleWithWebIdentityInput{
+		RoleArn:          &roleArn,
+		WebIdentityToken: &token,
+		RoleSessionName:  aws.String(fmt.Sprintf("%s-%s", workspaceID, userID)),
+	})
+	if err != nil {
+		logger.Err(err).Msg("Failed to retrieve S3 credentials")
+		return S3Credentials{}, err
+	}
+
+	return S3Credentials{
+		AccessKeyId:     *resp.Credentials.AccessKeyId,
+		SecretAccessKey: *resp.Credentials.SecretAccessKey,
+		SessionToken:    *resp.Credentials.SessionToken,
+		Expiration:      resp.Credentials.Expiration.UTC().Format(TimeFormat),
+	}, nil
 }
 
 // @Summary Request S3 session credentials
@@ -35,84 +94,28 @@ type STSClient interface {
 // @Failure 401 {object} string
 // @Failure 500 {object} string
 // @Router /workspaces/{workspace-id}/{user-id}/s3-tokens [post]
-func RequestS3CredentialsHandler(roleArn string, c STSClient,
-	k services.KeycloakClient) http.HandlerFunc {
-
+func RequestS3CredentialsHandler(roleArn string, c STSClient, k services.KeycloakClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		workspaceID := vars["workspace-id"]
-		userID := vars["user-id"]
+		logger := zerolog.Ctx(r.Context())
 
-		logger := zerolog.Ctx(r.Context()).With().Str("workspace", workspaceID).
-			Str("user", userID).Str("role arn", roleArn).Logger()
-
-		token, ok := r.Context().Value(middleware.TokenKey).(string)
-		if !ok {
-			err := "Invalid token"
-			http.Error(w, err, http.StatusUnauthorized)
-			logger.Error().Msg(err)
-			return
-		}
-
-		logger.Debug().Str("token", token).Msg("Token retrieved")
-
-		claims, ok := r.Context().Value(middleware.ClaimsKey).(authn.Claims)
-		if !ok {
-			err := "Invalid claims"
-			http.Error(w, err, http.StatusUnauthorized)
-			logger.Error().Msg(err)
-			return
-		}
-
-		logger = logger.With().Str("claims user", claims.Username).Logger()
-
-		if userID == "me" {
-			userID = claims.Username
-		}
-
-		if tokenExchangeRequired(claims, workspaceID, userID) {
-			logger.Info().Msg("Token exchange required")
-
-			// Exchange user scoped token for workspace scoped token
-			// TODO: Exchange Token will never change the user the token is for,
-			// to do this we need to implement a Keycloak Impersonate User API.
-			workspaceToken, err := k.ExchangeToken(token, fmt.Sprintf(
-				"workspace:%s", workspaceID))
-			if err != nil {
-				var errStatus int
-				if err, ok := err.(*services.HTTPError); ok {
-					errStatus = err.Status
-				} else {
-					errStatus = http.StatusInternalServerError
-				}
-				logger.Error().Err(err).Msg("Failed to get offline token")
-				http.Error(w, err.Error(), errStatus)
-				return
-			}
-
-			// Replace user scoped token with workspace scoped token
-			token = workspaceToken.Access
-		}
-
-		resp, err := c.AssumeRoleWithWebIdentity(r.Context(),
-			&sts.AssumeRoleWithWebIdentityInput{
-				RoleArn:          &roleArn,
-				WebIdentityToken: &token,
-				RoleSessionName:  aws.String(fmt.Sprintf("%s-%s", workspaceID, userID)),
-			})
+		creds, err := GetS3Credentials(roleArn, c, k, r)
 		if err != nil {
-			logger.Err(err).Msg("Failed to retrieve S3 credentials")
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			var status int
+			if httpErr, ok := err.(*services.HTTPError); ok {
+				status = httpErr.Status
+			} else {
+				status = http.StatusInternalServerError
+			}
+			http.Error(w, err.Error(), status)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(S3Credentials{
-			AccessKeyId:     *resp.Credentials.AccessKeyId,
-			SecretAccessKey: *resp.Credentials.SecretAccessKey,
-			SessionToken:    *resp.Credentials.SessionToken,
-			Expiration:      resp.Credentials.Expiration.UTC().Format(TimeFormat),
-		})
+		if err := json.NewEncoder(w).Encode(creds); err != nil {
+			logger.Error().Err(err).Msg("Failed to encode response")
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
 		logger.Info().Msg("S3 credentials retrieved")
 	}
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/EO-DataHub/eodhp-workspace-manager v0.0.0-20250204153111-916b8fae8a7b
 	github.com/apache/pulsar-client-go v0.14.0
 	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go-v2 v1.36.3
 	github.com/aws/aws-sdk-go-v2/config v1.29.1
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.54
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.78.2
 	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.34.13
 	github.com/aws/aws-sdk-go-v2/service/sesv2 v1.43.1
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.9
@@ -38,15 +41,16 @@ require (
 	github.com/EO-DataHub/eodhp-workspace-controller v0.0.0-20250129163210-6dc81f5c1b3c // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/ardielle/ardielle-go v1.5.2 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.17.54 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.24 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.34 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.15 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.24.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.10 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
